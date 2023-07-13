dcb1ad2c1ea6c642c8aa6696b0161032e2de2258
hoangakccm <vuhuyhoangktmt@gmail.com>
2023-07-13 15:11:04 +0700

    commit uploadfile2

--- app/src/main/java/com/example/testapi/ApiInterface.kt
+++ app/src/main/java/com/example/testapi/ApiInterface.kt
@@ -1,5 +1,7 @@
 package com.example.testapi
 
+import okhttp3.MultipartBody
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -12,6 +14,14 @@ interface ApiInterface {
     @PUT("data")
     fun putData(@Body myDataItem: MyDataItem): Call<MyDataItem>
 
+
+
+    @POST("updata")
+    fun postData(@Body postData: MyDataItem): Call<MyDataItem>
+
+    @Multipart
     @POST("update")
-    fun postData(@Body myDataItem: MyDataItem): Call<MyDataItem>
+    fun uploadFile(
+        @Part filePart: MultipartBody.Part
+    ): Call<ResponseBody>
 }
